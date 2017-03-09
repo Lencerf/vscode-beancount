c6a0bf6e13f2c2f91022cd6fecaa36712268736b
Lencerf <changyuan.lv@gmail.com>
2017-03-09 13:41:25 -0500

    align commodity when a decimal is inserted

--- src/extension.ts
+++ src/extension.ts
@@ -21,42 +21,7 @@ export function activate(context: vscode.ExtensionContext) {
         vscode.window.showInformationMessage('Hello World!');
     });
 
-    function alignSingleLine(line: number) {
-        const amountRegex = /([\-|\+]?)((?:\d|\d[\d,]*\d)?)(\.\d*)\s*([A-Z][A-Z0-9\'\.\_\-]{0,22}[A-Z0-9])/
-        let activeEditor = vscode.window.activeTextEditor;
-        let lineText = activeEditor.document.lineAt(line).text;
-        var resultArray = amountRegex.exec(lineText);
-        if (resultArray != null) {// && accountArray != null) {
-            // add 's' to the begin of a transation
-            let contentBefore = ("s" + lineText.substring(0, resultArray.index)).trim().substring(1);
-            var contentAfterAmount = ""
-            if (resultArray.index + resultArray[0].length < lineText.length) {
-                contentAfterAmount = lineText.substring(resultArray.index + resultArray[0].length)
-            }
-            //console.log("comments", contentAfterAmount, "resultArray.index=", resultArray.index)
-            let dotPosition = resultArray.index + resultArray[1].length + resultArray[2].length;
-            //console.log(lineText[dotPosition], dotPosition);
-            // find the position to insert spaces
-            // zero-based decimal position
-            let targetDecimalPosition = vscode.workspace.getConfiguration("beancount")["separatorColumn"] - 1;
-            let whiteLength = targetDecimalPosition - contentBefore.length - (resultArray[1].length + resultArray[2].length)
-            if (whiteLength > 0) {
-                let newLineText = contentBefore + (new Array(whiteLength + 1).join(' ')) + resultArray[0] + contentAfterAmount
-                let r = new vscode.Range(new vscode.Position(line,0), new vscode.Position(line, lineText.length))
-                let edit = new vscode.TextEdit(r, newLineText)
-                let wEdit = new vscode.WorkspaceEdit();
-                wEdit.set(activeEditor.document.uri, [edit]);
-                vscode.workspace.applyEdit(wEdit);
-            } else {
-                return
-            }
-
-        } else {
-            return
-        }
-    }
-
-    context.subscriptions.push(vscode.commands.registerCommand('extension.alignCommodity',()=>{
+    vscode.commands.registerCommand('extension.alignCommodity',()=>{
         // Align commodity for all lines
         let lc = vscode.window.activeTextEditor.document.lineCount;
         for (var i = 0; i < lc; i++) {
@@ -65,12 +30,83 @@ export function activate(context: vscode.ExtensionContext) {
                 alignSingleLine(i)
             }
         }
-    }))
+    })
 
+    let controller = new AlignCommodityController()
+    context.subscriptions.push(controller);
 
     context.subscriptions.push(disposable);
 }
 
+function alignSingleLine(line: number) {
+    let activeEditor = vscode.window.activeTextEditor
+    let originalText = activeEditor.document.lineAt(line).text
+    // save the original text length and cursor position
+    const originalLength = originalText.length
+    const originalCursorPosition = vscode.window.activeTextEditor.selection.active
+    // find an account name first
+    const accountRegex = /([A-Z][A-Za-z0-9\-]+)(:)/
+    var accountArray = accountRegex.exec(originalText)
+    if (accountArray == null) { return } // A commodity record always accors with an account.
+    // find a number with a decimal point
+    const amountRegex = /([\-|\+]?)((?:\d|\d[\d,]*\d)?)(\.)/
+    var amountArray = amountRegex.exec(originalText)
+    if (amountArray == null) { return }
+    let contentBefore = ("s" + originalText.substring(0, amountArray.index)).trim().substring(1);
+    var contentAfterAmount = ""
+    if (amountArray.index + amountArray[0].length < originalText.length) {
+        // get all the contents after the decimal point
+        contentAfterAmount = originalText.substring(amountArray.index + amountArray[0].length)
+    }
+    let dotPosition = amountArray.index + amountArray[0].length - 1
+    let targetDotPosition = vscode.workspace.getConfiguration("beancount")["separatorColumn"] - 1;
+    let whiteLength = targetDotPosition - contentBefore.length - (amountArray[1].length + amountArray[2].length)
+    if (whiteLength > 0) {
+        let newText = contentBefore + (new Array(whiteLength + 1).join(' ')) + amountArray[0] + contentAfterAmount
+        let r = new vscode.Range(new vscode.Position(line,0), new vscode.Position(line, originalText.length))
+        let edit = new vscode.TextEdit(r, newText)
+        let wEdit = new vscode.WorkspaceEdit();
+        wEdit.set(activeEditor.document.uri, [edit]);
+        vscode.workspace.applyEdit(wEdit);
+        // move cursor position
+        
+        if (line == originalCursorPosition.line) {
+            var newPosition = new vscode.Position(originalCursorPosition.line, 1 + originalCursorPosition.character + newText.length - originalLength)
+            //console.log(originalCursorPosition, newPosition)
+            activeEditor.selection = new vscode.Selection(newPosition, newPosition)
+        }
+    }
+}
+
 // this method is called when your extension is deactivated
 export function deactivate() {
+}
+
+/**
+ * AlignCommodityController
+ */
+class AlignCommodityController {
+    private _disposable: vscode.Disposable;
+
+    constructor() {
+        let subscriptions: vscode.Disposable[] = [];
+        vscode.workspace.onDidChangeTextDocument(this._onEvent, this, subscriptions);
+
+        this._disposable = vscode.Disposable.from(...subscriptions);
+    }
+
+    dispose() {
+        this._disposable.dispose();
+    }
+
+    private _onEvent(e: vscode.TextDocumentChangeEvent) {
+        console.log(e.contentChanges);
+        let text = e.contentChanges[0].text;
+        let rangeLength = e.contentChanges[0].rangeLength;
+        let line = e.contentChanges[0].range.start.line;
+        if (text == "." && rangeLength == 0) {
+            // the user just inserted a new decimal point
+            alignSingleLine(line) 
+        }
+    }
 }
